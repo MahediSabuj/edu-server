3fe789e1533d73dcbf57ec3ce6888bbfe6ada4eb
Mahedi Sabuj <sabuj.ict.mbstu@gmail.com>
2024-11-24 22:43:40 +0600

    initial commit

---
+++ src/main/java/com/ms29/edu/EduServerApplication.java
@@ -0,0 +1,15 @@
+package com.ms29.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EduServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ms29/edu/EduServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms29.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
